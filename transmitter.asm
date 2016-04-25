fa951d77a69c583ee51262e1ad289ae7f36fb5db
chirath <chirath.02@gmail.com>
2016-04-26 00:45:13 +0530

    Completed transmission with complete comments

--- transmitter.asm
+++ transmitter.asm
@@ -1,92 +1,112 @@
+
+
+
 List P= 16F877A
 #include P16F877A.inc
 
-BANKSEL TRISB
-MOVLW 0X78
-MOVWF TRISB
-MOVLW 0XC0
-MOVWF TRISC
-
-
-;---CONFIGURE SPBRG FOR DESIRED BAUD RATE
-MOVLW D'51' ;WE WILL USE 4800bps 
-MOVWF SPBRG ;BAUD AT 4MHZ
-
+MAIN:
+	CALL KEYBOARD_CONFIG
+	CALL KEYBOARD
+	CALL TRANSMISSION_CONFIG
 	
-;---CONFIGURE TXSTA
-MOVLW B'00100100' ;CONFIGURE TXSTA AS :
-MOVWF TXSTA ;
-;8 BIT TRANSMISSION - 6.BIT
-;TRANSMIT ENABLED - 5.BIT
-;ASYNCHRONOUS MODE - 4.BIT
-;ENABLE HIGH SPEED BAUD RATE - 2.BIT
+	BANKSEL TXSTA 
+	BSF TXSTA, 5 ; Set TXEN, enable transmission
+	
+	BANKSEL TXREG 
+	MOVF 0X20, 0 
+	MOVWF TXREG ;load keyboard value into TXREG for transmission
 
-BCF STATUS, RP0 ;SWITCH TO BANK 00
+	; Trasmission starts....
+	
+	BANKSEL TXSTA
+	
+	LOOP:
+		BTFSC TXSTA, 5 ; poll enable bit, TXEN becomes 0 after transmission.
+		GOTO LOOP
+	
+	GOTO TERMINATE ; Goto END
 
-BANKSEL PORTB
-CLRF PORTB
+TRANSMISSION_CONFIG:
+	MOVLW 0XC0
+	MOVWF TRISC
+	
+	;---CONFIGURE SPBRG FOR DESIRED BAUD RATE
+	MOVLW D'51' ;WE WILL USE 4800bps 
+	MOVWF SPBRG ;BAUD AT 4MHZ
+	
+		
+	;---CONFIGURE TXSTA
+	MOVLW B'00100100' ;CONFIGURE TXSTA AS :
+	MOVWF TXSTA ;
+	;8 BIT TRANSMISSION - 6.BIT
+	;TRANSMIT ENABLED - 5.BIT
+	;ASYNCHRONOUS MODE - 4.BIT
+	;ENABLE HIGH SPEED BAUD RATE - 2.BIT
 
+	RETURN
 
-START:
+KEYBOARD_CONFIG:
+	; Keyboard input output config
+	BANKSEL TRISB
+	MOVLW 0X78 
+	MOVWF TRISB
+	
+	; RB6, RB5, RB4, RB3 is input for rows.
+	; RB2, RB1, RB0 is output for columns
+	; RB7 unimplimented]
 
-MOVLW 0XFF
-MOVWF 0X20
+	RETURN
 
-MOVLW 0X01
-MOVWF PORTB
-CALL SWITCH
-MOVWF 0X20
-BTFSS 0X20, 7
-ADDLW 0X03
+KEYBOARD: ; Keyboard module starts
+	BANKSEL PORTB ; Keyboard connected to portb
+	CLRF PORTB ; clear portb for input and output
+	
+	MOVLW 0XFF  ; make all bits 1 for chevking keypress
+	MOVWF 0X20  ; data memory adress for storing keyboard value
+	
+	; sent high to col 3
 
-MOVLW 0X02
-MOVWF PORTB
-CALL SWITCH
-MOVWF 0X20
-BTFSS 0X20, 7
-ADDLW 0X02
+	MOVLW 0X01 
+	MOVWF PORTB
+	CALL SWITCH ; check for row and return row number
+	MOVWF 0X20
+	BTFSS 0X20, 7
+	ADDLW 0X03
 
-MOVLW 0X04
-MOVWF PORTB
-CALL SWITCH
-MOVWF 0X20
-BTFSS 0X20, 7
-ADDLW 0X01
+	; sent high to col 2
+	
+	MOVLW 0X02
+	MOVWF PORTB
+	CALL SWITCH ; check for row and return row number
+	MOVWF 0X20
+	BTFSS 0X20, 7
+	ADDLW 0X02
 
-MOVWF 0X20
+	; sent high to col 1	
 
-BTFSC 0X20, 7
-GOTO START
-
-
-BANKSEL TXSTA
-BSF TXSTA, 5
-
-BANKSEL TXREG
-MOVF 0X20, 0
-MOVWF TXREG
-
-BANKSEL TXSTA
-
-LOOP:
-	BTFSC TXSTA, 5
-	GOTO LOOP
-
-
-
-GOTO TERMINATE
-
-
-SWITCH:
-	BTFSC PORTB, 6
-	RETLW 0X00
-	BTFSC PORTB, 5
-	RETLW 0X03
-	BTFSC PORTB, 4
-	RETLW 0X06
-	BTFSC PORTB, 3
-	RETLW 0X09
-	RETLW 0XF0
+	MOVLW 0X04
+	MOVWF PORTB
+	CALL SWITCH ; check for row and return row number
+	MOVWF 0X20
+	BTFSS 0X20, 7
+	ADDLW 0X01
+	
+	MOVWF 0X20 ;final value of key stored at 0x20, 0xff if no key is pressed.
+	
+	BTFSS 0X20, 7 ; check if key is pressed
+	RETURN ; Key pressed if 7th bit is 0
+	GOTO KEYBOARD ; recheck if key is pressed
+	
+	SWITCH:
+		BTFSC PORTB, 6 ; check row 1
+		RETLW 0X00 ; 0x00 returned if row = 1
+		BTFSC PORTB, 5 ; check row 2
+		RETLW 0X03 ; 0x00 returned if row = 2
+		BTFSC PORTB, 4 ; check row 3
+		RETLW 0X06 ; 0x00 returned if row = 3
+		BTFSC PORTB, 3 ; check row 4
+		RETLW 0X09 ; 0x00 returned if row = 4
+		RETLW 0XFF ; 0xFF returned if no key is pressed
 
 TERMINATE:
 END
